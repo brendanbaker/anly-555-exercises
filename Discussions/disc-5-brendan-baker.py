f4386ebad5a114d16a9d674e4b6e52e46da2226a
brendanbaker <brendansbaker@gmail.com>
2023-03-30 17:01:50 -0400

    initial functionality

--- Discussions/disc-5-brendan-baker.py
+++ Discussions/disc-5-brendan-baker.py
@@ -6,10 +6,10 @@ Brendan Baker
 Prompt: Consider an array implementation of a binary heap. Given the strict balance constraint, an array implementation is very efficient and effective!
 An n-ary heap is generalization of a Binary Heap that will allow for a branching factor of n.
 
-1. Edit the code below so that one can create a heap with any branching factor (eg 3-ary heaps, 5-ary heaps, ... ). (Take care to assure the indexing is correct!!) 
+1. Edit the code below so that one can create a heap with any branching factor (eg 3-ary heaps, 5-ary heaps, ... ). (Take care to assure the indexing is correct!!)
 2. Create a __str__() member similar to the one implemented in the in-class exercise that allows you to visualize the tree using the tool found at: http://mshang.ca/syntree/
 3. One main goal associated with trees is improved time complexity, but how is this related to the branching factor? For example, are 2-ary heaps more or less efficient than 5-ary heaps?
-Propose an optimal value for n and justify your proposition with an extensive discussion and explanation. 
+Propose an optimal value for n and justify your proposition with an extensive discussion and explanation.
 
 '''
 """
@@ -18,64 +18,94 @@ Code created on Thu Oct 15 12:32:31 2020
 @author: jerem
 """
 
-class BinHeap:
-    def __init__(self):
-        self.heapList = [0]
-        self.currentSize = 0
 
+class NaryHeap:
+    def __init__(self, n=2):
+        self.heapList = [0]  # Initialize list of elements
+        self.currentSize = 0  # Keeps track of size
+        self.branching_factor = n  # Branching factor attribute
 
-    def percUp(self,i):
-        while i // 2 > 0:
-          if self.heapList[i] < self.heapList[i // 2]:
-             tmp = self.heapList[i // 2]
-             self.heapList[i // 2] = self.heapList[i]
-             self.heapList[i] = tmp
-          i = i // 2
+    def percUp(self, i):
+        '''
+        Percolates the value up the heaplist at index i to maintain the heap property.
+        '''
+        while i // self.branching_factor > 0:
+            # Calculate the parent's index
+            parent = (i - 1)//self.branching_factor
+            # If the value at index i is less than the value at the parent's index
+            if self.heapList[i] < self.heapList[parent]:
+                # Swap the values
+                self.heapList[i], self.heapList[parent] = self.heapList[parent], self.heapList[i]
+            i = parent  # Set the index to be the parent
 
-    def insert(self,k):
-      self.heapList.append(k)
-      self.currentSize = self.currentSize + 1
-      self.percUp(self.currentSize)
+    def insert(self, k):
+        '''
+        Inserts a value into the heaplist.
+        '''
+        self.heapList.append(k) # Append the value to the end of the list
+        self.currentSize = self.currentSize + 1 # Increment the size
+        self.percUp(self.currentSize) # Percolate the value up the heaplist
 
-    def percDown(self,i):
-      while (i * 2) <= self.currentSize:
-          mc = self.minChild(i)
-          if self.heapList[i] > self.heapList[mc]:
-              tmp = self.heapList[i]
-              self.heapList[i] = self.heapList[mc]
-              self.heapList[mc] = tmp
-          i = mc
+    def percDown(self, i):
+        '''
+        Percolates a value down the heaplist at index i to maintain the heap property.
+        '''
+        while i*self.branching_factor - (self.branching_factor - 2) <= self.currentSize:
+            mc = self.minChild(i) # Find the index of the minimum child
+            if self.heapList[i] > self.heapList[mc]: 
+                self.heapList[i], self.heapList[mc] = self.heapList[mc], self.heapList[i]  # Swap values down
+            i = mc
 
-    def minChild(self,i):
-      if i * 2 + 1 > self.currentSize:
-          return i * 2
-      else:
-          if self.heapList[i*2] < self.heapList[i*2+1]:
-              return i * 2
-          else:
-              return i * 2 + 1
+    def minChild(self, i):
+        '''
+        Finds the index of the minimum child of the value at index i in the heapList.
+        '''
+        minIndex = i * self.branching_factor - (self.branching_factor - 2)  # Set initial minimum index
+        for j in range(1, self.branching_factor):
+            childIndex = i * self.branching_factor - (self.branching_factor - 2) + j  # Calculate child index
+            if childIndex <= self.currentSize and self.heapList[childIndex] < self.heapList[minIndex]:
+                minIndex = childIndex  # Update minimum index
+        return minIndex
 
     def delMin(self):
-      retval = self.heapList[1]
-      self.heapList[1] = self.heapList[self.currentSize]
-      self.currentSize = self.currentSize - 1
-      self.heapList.pop()
-      self.percDown(1)
-      return retval
+        '''
+        Deletes the minimum index of the heap
+        '''
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
 
     def buildHeap(self,alist):
-      i = len(alist) // 2
-      self.currentSize = len(alist)
-      self.heapList = [0] + alist[:]
-      while (i > 0):
-          self.percDown(i)
-          i = i - 1
+        '''
+        Constructs the heap based on the list of elements.
+        '''
+        i = (len(alist) + self.branching_factor - 2) // self.branching_factor # Starting index
+        self.currentSize = len(alist) # Set the current size attribute
+        self.heapList = [0] + alist[:] # Set the heapList attribute
+        while (i > 0):
+            self.percDown(i)
+            i = i - 1
+            
+    def __str__(self):
+        '''
+        Prints the heap in a tree format
+        '''
+        return(self.printString())
+    
+    def printString(self):
+        return(str(self.heapList))
 
-bh = BinHeap()
-bh.buildHeap([9,5,6,2,3])
 
-print(bh.delMin())
-print(bh.delMin())
-print(bh.delMin())
-print(bh.delMin())
-print(bh.delMin())
+if __name__ == '__main__':
+    bh = NaryHeap()
+    bh.buildHeap([9,5,6,2,3])
+
+    print(bh)
+    print(bh.delMin())
+    print(bh.delMin())
+    print(bh.delMin())
+    print(bh.delMin())
+    print(bh.delMin())
